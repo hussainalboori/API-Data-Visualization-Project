d477e638f1b7d4eac993a86331a882583ca4f9ca
hnabeel <hnabeel3@gmail.com>
2023-09-12 18:26:19 +0300

    server

---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./web/static"))))
+	log.Println("Connect to our website throught http://Local host:4000")
+	http.ListenAndServe(":4000", nil)
+}
